d1bfc5526cc7cd77b9eea52e3e2cb310b20199d9
Devin Setiawan <90437494+DevinRS@users.noreply.github.com>
2024-08-02 01:09:55 -0500

    Add files via upload

---
+++ Synthetic Dataset/synthetic_dataset3.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import numpy as np
+
+# X is 100 variables following a normal distribution rnaging from 0 to 1
+X = np.random.uniform(0, 0.5, size=50)
+X = np.append(X, np.random.uniform(0.5, 1, size=50))
+
+# Assign random labels to the variables either 0 or 1
+target = []
+for i in range(100):
+    target.append(np.random.randint(0, 2))
+
+# Feature 1, if X is less than 0.7 and target is 0 it is below the threshold hyperplane
+# if X is less than 0.7 and target is 1 it is above the threshold hyperplane
+# if X is greater than 0.7 and target is 0, append noise
+# if X is greater than 0.7 and target is 1, append noise
+feature_1 = []
+for i in range(len(X)):
+    if X[i] < 0.7 and target[i] == 0:
+        # append random number between 0 and 0.5
+        feature_1.append(np.random.uniform(0, 0.5))
+    elif X[i] < 0.7 and target[i] == 1:
+        feature_1.append(np.random.uniform(0.5, 1))
+    elif X[i] > 0.7 and target[i] == 0:
+        feature_1.append(np.random.uniform(0, 1))
+    elif X[i] > 0.7 and target[i] == 1:
+        feature_1.append(np.random.uniform(0, 1))
+
+# Feature 2, if X is more than 0.7 and target is 0 it is below the threshold hyperplane
+# if X is more than 0.7 and target is 1 it is above the threshold hyperplane
+# if X is less than 0.7 and target is 0, append noise
+# if X is less than 0.7 and target is 1, append noise
+feature_2 = []
+for i in range(len(X)):
+    if X[i] > 0.3 and target[i] == 0:
+        feature_2.append(np.random.uniform(0, 0.5))
+    elif X[i] > 0.3 and target[i] == 1:
+        feature_2.append(np.random.uniform(0.5, 1))
+    elif X[i] < 0.3 and target[i] == 0:
+        feature_2.append(np.random.uniform(0, 1))
+    elif X[i] < 0.3 and target[i] == 1:
+        feature_2.append(np.random.uniform(0, 1))
+
+# Plot X vs feature_1 with target labels
+import matplotlib.pyplot as plt
+plt.scatter(X, feature_1, c=target)
+plt.xlabel('X')
+plt.ylabel('Feature 1')
+# draw vertical line at x = 0.7
+plt.axvline(x=0.7, color='r', linestyle='--')
+plt.show()
+
+# Plot X vs feature_2 with target labels
+plt.scatter(X, feature_2, c=target)
+plt.xlabel('X')
+plt.ylabel('Feature 2')
+# draw vertical line at x = 0.7
+plt.axvline(x=0.3, color='r', linestyle='--')
+plt.show()
+
+# Convert to pandas dataframe
+df = pd.DataFrame({'X': X, 'Feature 1': feature_1, 'Feature 2': feature_2, 'Target': target})
+
+# Save to csv
+df.to_csv('synthetic_dataset3.csv', index=False)
+
+
---
+++ Synthetic Dataset/synthetic_dataset5.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+
+# X is 100 variables following a normal distribution rnaging from 0 to 1
+X = np.random.uniform(0, 0.5, size=50)
+X = np.append(X, np.random.uniform(0.5, 1, size=50))
+
+# Assign random labels to the variables either 0 or 1
+target = []
+for i in range(100):
+    target.append(np.random.randint(0, 2))
+
+# Feature 1, if X is greater than 0.7 append random noise
+# if X is less than 0.7 and target is 0 it is above the threshold hyperplane
+# if X is less than 0.7 and target is 1 it is below the threshold hyperplane
+feature_1 = []
+for i in range(len(X)):
+    if X[i] > 0.7:
+        feature_1.append(np.random.uniform(0, 1))
+    elif X[i] <= 0.7 and target[i] == 0:
+        #90% chance of being 1
+        feature_1.append(np.random.choice([np.random.uniform(0, 0.5), np.random.uniform(0.5, 1)], p=[0.1, 0.9]))
+    elif X[i] <= 0.7 and target[i] == 1:
+        #90% chance of being 0
+        feature_1.append(np.random.choice([np.random.uniform(0, 0.5), np.random.uniform(0.5, 1)], p=[0.9, 0.1]))
+
+# Feature 2, is noise
+feature_2 = []
+for i in range(len(X)):
+    feature_2.append(np.random.uniform(0, 1))
+
+# Feature 3, is noise
+feature_3 = []
+for i in range(len(X)):
+    feature_3.append(np.random.uniform(0, 1))
+
+
+
+# Plot X vs feature_1 with target labels
+import matplotlib.pyplot as plt
+plt.scatter(X, feature_1, c=target)
+plt.xlabel('X')
+plt.ylabel('Feature 1')
+plt.show()
+
+# Plot X vs feature_2 with target labels
+plt.scatter(X, feature_2, c=target)
+plt.xlabel('X')
+plt.ylabel('Feature 2')
+plt.show()
+
+# Plot X vs feature_3 with target labels
+plt.scatter(X, feature_3, c=target)
+plt.xlabel('X')
+plt.ylabel('Feature 3')
+plt.show()
+
+
+# Convert to pandas dataframe
+df = pd.DataFrame({'X': X, 'Feature 1': feature_1, 'Feature 2': feature_2, 'Feature 3': feature_3, 'Target': target})
+
+# Save to csv
+df.to_csv('synthetic_dataset5.csv', index=False)
+
+
